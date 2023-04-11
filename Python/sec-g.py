9a304d52725b2260399383ac2af6b967c3bcdda3
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2023-04-11 17:47:36 +0000

    q

--- Python/sec-g.py
+++ Python/sec-g.py
@@ -1,5 +1,11 @@
 # demonstrate working with dictionaries
 dict1 = {1: 'Mridul', 2: 'Sakshi', 3: 'Aashna', 4: 'Pawan'}
 print(dict1)
-dict1 [5] = 'Lakshay' #added to dict1
+dict1[5] = 'Lakshay'  # added to dict1
 print(dict1)
+dict1[5] = 'Lokii'  # edited dict1
+print(dict1)
+print(dict1[2])  # accessing element
+del dict1[5] #delete element
+print(dict1)
+ 
