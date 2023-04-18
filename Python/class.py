cbe4392e24cb2b66a47dbd3cd8ddbd9502ac66be
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2023-04-18 17:39:04 +0000

    q

--- Python/class.py
+++ Python/class.py
@@ -1,19 +1,28 @@
-s = "python programming"
-print (s)
-x = bool(s.islower())
-y = bool(s.endswith('ing'))
-print(y)
-s1 ='hello'
-s2 ='Hello'
-s3 ='HELLO'
+import math
+# Grades = {'Sahil':90,'Abhijeet':65}
+# print(Grades.keys())
+# print(Grades.values())
+# print(len(Grades))
+# Grades['Kuruss']=99
+# print(Grades)
+# Grades['Abhijeet'] += 5
+# print(Grades)
+# del Grades['Abhijeet']
+# print(Grades)
+# print(Grades.items())
+# dic1={}
+# i=int(input("Enter the number of countries you want to enter: "))
+# while(i>0):
+#     country=input("Enter the country name: ")
+#     captial=input("Enter the capital name: ")
+#     dic1[country]=captial
+#     i-=1
+# print(dic1)
 
-x = int(input("Enter the value of x: "))
-n = int(input("Enter the value of n: "))
-sum = 0
-
-for i in range(0, n + 1) :
-    term = x**i
-    print(term)
-    sum += term
-
-print("Sum =", sum)
+#convert milliseconds to hours, minutes and seconds
+milli=int(input("Enter time in milliseconds "))
+sec=(milli//1000)
+minu=(sec//60)
+hrs=(minu//60)
+sec%=60;minu%=60;hrs%=24
+print(hrs,":",minu,":",sec)
--- Python/sec-h.py
+++ Python/sec-h.py
@@ -7,25 +7,46 @@ n1, n2 = n2, n1
 print('After swapping: (', n1, ',', n2, ')')
 
 
-#calculate arithmetic operation on two number using user defined function
+# calculate arithmetic operation on two number using user defined function
 
 
-#Calculate diameter and area of circle using user defined function
+# Calculate diameter and area of circle using user defined function
 r = float(input('Enter the radius of circle: '))
+
+
 def circle(r):
     d = float(2 * r)
     area = float(math.pi * r * r)
     return d, area
+
+
 d, area = circle(r)
 print('Diameter of circle: ', d, '\tArea of circle: ', area)
 
 
-#function that takes three numbers as parameters, and returns the median value of those parameters as its result. Include a main program that reads three values from the user and displays their median
-a=int(input('Enter the first number: '))
-b=int(input('Enter the second number: '))
-c=int(input('Enter the third number: '))
-def median(a,b,c):
-    t1=[a,b,c]
+# function that takes three numbers as parameters, and returns the median value of those parameters as its result. Include a main program that reads three values from the user and displays their median
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+c = int(input('Enter the third number: '))
+
+
+def median(a, b, c):
+    t1 = [a, b, c]
     t1.sort()
     return t1[1]
-print ('The median is:', median(a,b,c))
+print('The median is:', median(a, b, c))
+
+
+# Program to filter even values from list using lambda function
+l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print('List: ', l1)
+evel1 = list(filter(lambda x: x % 2 == 0, l1))
+print('Even values from list: ', evel1)
+
+
+#sum of elements of a list using lambda function
+l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum = sum(lambda i, j: i + j, l1)
+print('Sum of elements of list: ', sum)
+
+#find small number between two numbers using Lambda function
